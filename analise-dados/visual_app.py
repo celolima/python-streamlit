dadf61a92b75728eebfb3f701502b2e4abde292d
Marcelo Lima <marcelo@trt3.jus.br>
2025-08-21 11:49:12 -0300

    Adiciona visualização via Streamlit

--- analise-dados/visual_app.py
+++ analise-dados/visual_app.py
@@ -5,4 +5,20 @@ path_compras = "datasets/compras.csv"
 
 df_compras = pd.read_csv(path_compras,decimal=",",sep=";")
 
-st.dataframe(df_compras)
+colunas = list(df_compras.columns.sort_values())
+colunas_selecionadas = st.sidebar.multiselect("Colunas:", colunas, colunas)
+
+col1, col2 = st.sidebar.columns(2)
+col_filtro = col1.selectbox('Selecione a coluna', [c for c in colunas if c not in ["id_compra"]])
+
+valor_filtro = col2.selectbox("Selecione o valor", list(df_compras[col_filtro].unique()))
+
+st_filtrar = col1.button('Filtrar')
+st_limpar = col2.button('Limpar')
+
+if st_filtrar:
+    st.dataframe(df_compras.loc[df_compras[col_filtro] == valor_filtro, colunas_selecionadas])
+elif st_limpar:
+    st.dataframe(df_compras[colunas_selecionadas])
+else:
+    st.dataframe(df_compras[colunas_selecionadas])
