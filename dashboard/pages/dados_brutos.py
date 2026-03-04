9deee7de5ee3a3e02758aa2188237548c1e69eb1
Marcelo Lima <marcelo@trt3.jus.br>
2026-03-04 15:09:25 -0300

    Adiciona pagina arquivo bruto

---
+++ dashboard/pages/dados_brutos.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import requests
+import pandas as pd
+from dashboard import DATA_COMPRA
+
+st.title('Dados Brutos')
+st.set_page_config(layout='wide')
+
+url = 'https://labdados.com/produtos'
+response = requests.get(url, verify=False)
+dados = pd.DataFrame.from_dict(response.json())
+dados[DATA_COMPRA] = pd.to_datetime(dados[DATA_COMPRA], format='%d/%m/%Y')
+
+with st.expander('Colunas'):
+    colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
+
+st.sidebar.title('Filtros')
+with st.sidebar.expander('Nome do produto'):
+    produtos = st.multiselect('Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())    
+with st.sidebar.expander('Preço do produto'):
+    preco = st.slider('Selecione o preço', 0, 5000, (0,5000))
+with st.sidebar.expander('Data da compra'):
+    data_compra = st.date_input('Selecione a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
+        
+st.dataframe(dados)
